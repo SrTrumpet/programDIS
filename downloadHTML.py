7c60f0bd219620905bc1715616085a426682f6ff
Elias M Olivares <elias.manque.o@gmail.com>
2025-01-18 17:56:11 -0300

    feat: se añadio el webdriver para el navegador edge y se solucionaron errores al momento de convertir de docx a html, se agrego opciones al dar click derecho, la tabla de referencias muestra al nombre real del archivo y el nickname que define el usuario

--- downloadHTML.py
+++ downloadHTML.py
@@ -7,49 +7,56 @@ from selenium.webdriver.edge.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-# Configuración del navegador Edge
-edge_options = Options()
-edge_options.add_experimental_option('prefs', {
-    "download.default_directory": os.path.join(os.path.expanduser("~"), "Desktop", "html"),  # Ruta de descargas
-    "download.prompt_for_download": False,
-    "download.directory_upgrade": True,
-    "safebrowsing.enabled": True
-})
-webdriver_path = "D:/msedgedriver.exe"
-driver = webdriver.Edge(service=Service(webdriver_path), options=edge_options)
+class DownloadHTML():
 
-# Rutas de carpetas
-docx_directory = "C:/Users/Elias M. Olivares/Desktop/Script DIS"
-output_directory = os.path.join(os.path.expanduser("~"), "Desktop", "html")
-if not os.path.exists(output_directory):
-    os.makedirs(output_directory)
+    def downloadHtml(pathWork):
+        edge_options = Options()
+        edge_options.add_experimental_option('prefs', {
+            "download.default_directory": os.path.abspath("./download"),
+            "download.prompt_for_download": False,
+            "download.directory_upgrade": True,
+            "safebrowsing.enabled": True
+        })
+        webdriver_path = "./webdriver/msedgedriver.exe"
+        driver = webdriver.Edge(service=Service(webdriver_path), options=edge_options)
 
-try:
-    # Iterar sobre los archivos .docx
-    for filename in os.listdir(docx_directory):
-        if filename.endswith(".docx"):
-            file_path = os.path.join(docx_directory, filename)
-            
-            # Navegar al sitio web
-            driver.get("https://products.groupdocs.app/es/conversion/docx-to-html")
-            time.sleep(6)  # Esperar a que cargue la página
-            
-            # Subir archivo
-            upload_input = driver.find_element(By.XPATH, "//input[@type='file']")
-            upload_input.send_keys(file_path)
-            time.sleep(20)  # Esperar a que se procese la carga
-            
-            # Convertir el archivo
-            convert_button = driver.find_element(By.XPATH, "//button[contains(text(), 'Convertir ahora')]")
-            convert_button.click()
-            time.sleep(20)  # Esperar a que se procese la conversión
-            
-            # Descargar el archivo
-            download_link = WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable((By.XPATH, "//a[@id='convertDownload']"))
-            )
-            download_link.click()
-            time.sleep(10)  # Esperar a que se descargue el archivo
+        # Rutas de carpetas
+        docx_directory = pathWork
+        output_directory = "./download"
+        if not os.path.exists(output_directory):
+            os.makedirs(output_directory)
 
-finally:
-    driver.quit()
+        try:
+            # Iterar sobre los archivos .docx
+            for filename in os.listdir(docx_directory):
+                if filename.endswith(".docx"):
+                    file_path = os.path.join(docx_directory, filename)
+                    
+                    # Navegar al sitio web
+                    driver.get("https://products.groupdocs.app/es/conversion/docx-to-html")
+                    time.sleep(6)  # Esperar a que cargue la página
+                    
+                    # Subir archivo
+                    upload_input = driver.find_element(By.XPATH, "//input[@type='file']")
+                    upload_input.send_keys(file_path)
+                    time.sleep(20)  # Esperar a que se procese la carga
+                    
+                    convert_button = driver.find_element(By.XPATH, "//button[contains(text(), 'Convertir ahora')]")
+                    driver.execute_script("arguments[0].scrollIntoView();", convert_button)  # Desplazarse hasta el boton
+                    time.sleep(1)  # Esperar un momento para asegurarse de que el scroll se haya completado
+                    convert_button.click()
+                    time.sleep(20)
+                    
+                    # Descargar el archivo
+                    download_link = WebDriverWait(driver, 10).until(
+                        EC.element_to_be_clickable((By.XPATH, "//a[@id='convertDownload']"))
+                    )
+                    driver.execute_script("arguments[0].scrollIntoView();", download_link)  # Desplazarse hasta el enlace de descarga
+                    time.sleep(1)
+                    download_link.click()
+                    time.sleep(10) # Esperar a que se descargue el archivo
+        finally:
+            driver.quit()
+
+
+
--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 from tkinter import *
-from tkinter import filedialog
-from os import path, listdir, makedirs, getcwd
+from tkinter import filedialog, simpledialog, messagebox, ttk
+from os import path, listdir, makedirs, getcwd, startfile
 import shutil
 import sqlite3
+from downloadHTML import DownloadHTML
 
 ################## CONFIGURACIONES INICIALES ##################
 root = Tk()
-root.title("DIS Coquimbo - Gestor de documentacion")#Titulo de la ventana
-root.geometry("1280x720")# Tamaño de la ventana
-root.iconbitmap("./img/ucnLogo.ico")#Icono de la ventana
-directoryEnterprise = "" # Directorio donde se van a sacar los nombres de los archivos generados por el Enterprise Architech
-directoryWork = "" # Directorio donde se van a sacar los nombres de los archivos generados por el trabajo
-directoryOriginal = "" # Directorio donde se van a sacar los nombres de los archivos originales
+root.title("DIS Coquimbo - Gestor de documentacion")  # Titulo de la ventana
+root.geometry("1280x720")  # Tamaño de la ventana
+root.iconbitmap(default="./img/ucnLogo.ico")  # Icono de la ventana
+directoryEnterprise = ""  # Directorio donde se van a sacar los nombres de los archivos generados por el Enterprise Architech
+directoryWork = ""  # Directorio donde se van a sacar los nombres de los archivos generados por el trabajo
+directoryOriginal = ""  # Directorio donde se van a sacar los nombres de los archivos originales
 messageError = None
 messageSuccess = None
-tagColumns = ("Seleccionar Filtro","Administrar plataformas TI", "Entregar soporte a usuarios", "Gestion de outsourcing")# Tupla usada para luego usarlas de Tag en la base de datos
-tagSubColumns = ("Seleccionar SubFiltro","Implementar Servicios", "Monitoreo de servicios", "Respaldo de servicios", "Planes de contingencia", "Mantenciones", "a sus cuentas", "a sus equipos", "a plataformas", "a eventos")
+textPathWork = None
+tagColumns = ("Seleccionar Filtro", "Administrar plataformas TI", "Entregar soporte a usuarios", "Gestion de outsourcing")  # Tupla usada para luego usarlas de Tag en la base de datos
+tagSubColumns = ("Seleccionar SubFiltro", "Implementar Servicios", "Monitoreo de servicios", "Respaldo de servicios", "Planes de contingencia", "Mantenciones", "a sus cuentas", "a sus equipos", "a plataformas", "a eventos")
 originalDir = path.join(getcwd(), "original")
 databaseDir = path.join(getcwd(), "database")
 enterpriseDir = path.join(getcwd(), "enterprise")
+downloadDir = path.join(getcwd(), "download")
 #################################################################
 
 def getPath():
@@ -35,7 +38,7 @@ def getPath():
             messageError.destroy()
             messageError = None
 
-scannButton = Button(root, text="Definir el directorio",font=("Arial"),width=30, height=2, bg="blue", fg="white", justify="center", command=getPath)
+scannButton = Button(root, text="Definir el directorio", font=("Arial"), width=30, height=2, bg="blue", fg="white", justify="center", command=getPath)
 scannButton.grid(row=0, column=0, padx=10, pady=10)
 ###################
 
@@ -51,57 +54,71 @@ def saveReferences():
                 try:
                     shutil.copy(path.join(directoryEnterprise, fileName), enterpriseDir)
                     fileSize = path.getsize(path.join(directoryEnterprise, fileName))
-                    print("Nombre:",fileName,"tamaño:", fileSize)
-                    cursor.execute("INSERT INTO fileReferences(fileName, nickName, tag, subTag, size) VALUES(?,?,?,?,?)", (fileName, "", "", "",fileSize))
+                    print("Nombre:", fileName, "tamaño:", fileSize)
+                    cursor.execute("INSERT INTO fileReferences(fileName, nickName, tag, subTag, size) VALUES(?,?,?,?,?)", (fileName, "", "", "", fileSize))
                     connectionDataBase.commit()
-                except(sqlite3.IntegrityError):
+                except sqlite3.IntegrityError:
                     shutil.copy(path.join(directoryEnterprise, fileName), enterpriseDir)
                     fileSize = path.getsize(path.join(directoryEnterprise, fileName))
-                    print("Nombre:",fileName,"tamaño:", fileSize)
+                    print("Nombre:", fileName, "tamaño:", fileSize)
         updateFileList("","")
 
-saveButton = Button(root, text="Guardar referencias",font=("Arial"),width=30, height=2, bg="green", fg="white", justify="center", command=saveReferences)
+saveButton = Button(root, text="Guardar referencias", font=("Arial"), width=30, height=2, bg="green", fg="white", justify="center", command=saveReferences)
 saveButton.grid(row=0, column=1, padx=10, pady=10)
 ################################################################
 
-def updateFileList(tag=NONE, subTag=NONE):
-    print('tag: ',tag, 'subTag: ',subTag)
-    fileListbox.delete(0, END)
-    if "Seleccionar Filtro" and subTag != "Seleccionar SubFiltro":
-        cursor.execute("SELECT fileName FROM fileReferences WHERE subTag = ? ORDER BY size DESC", (subTag,))
-    elif tag != "Seleccionar Filtro" and subTag == "Seleccionar SubFiltro":
-        cursor.execute("SELECT fileName FROM fileReferences WHERE tag = ? ORDER BY size DESC", (tag,))
-    elif subTag != "Seleccionar SubFiltro" and tag != "Seleccionar Filtro":
-        cursor.execute("SELECT fileName FROM fileReferences WHERE tag = ? AND subTag = ? ORDER BY size DESC", (tag, subTag))
+def updateFileList(tag=None, subTag=None):
+    print('tag: ', tag, 'subTag: ', subTag)
+    fileTreeview.delete(*fileTreeview.get_children())
+    if tag and tag != "Seleccionar Filtro" and subTag and subTag != "Seleccionar SubFiltro":
+        cursor.execute("SELECT fileName, nickName FROM fileReferences WHERE tag = ? AND subTag = ? ORDER BY size DESC", (tag, subTag))
+    elif tag and tag != "Seleccionar Filtro":
+        cursor.execute("SELECT fileName, nickName FROM fileReferences WHERE tag = ? ORDER BY size DESC", (tag,))
+    elif subTag and subTag != "Seleccionar SubFiltro":
+        cursor.execute("SELECT fileName, nickName FROM fileReferences WHERE subTag = ? ORDER BY size DESC", (subTag,))
     else:
-        cursor.execute("SELECT fileName FROM fileReferences ORDER BY size DESC")
+        cursor.execute("SELECT fileName, nickName FROM fileReferences ORDER BY size DESC")
 
     for row in cursor.fetchall():
-        fileListbox.insert(END, row[0])
+        fileTreeview.insert("", "end", values=row)
 
-fileListbox = Listbox(root, width=30, height=25, font=("Arial", 12))
-fileListbox.grid(row=2, column=0, padx=10, pady=10)
+fileTreeview = ttk.Treeview(root, columns=("fileName", "nickName"), show="headings", height=23)
+fileTreeview.heading("fileName", text="Nombre del archivo")
+fileTreeview.heading("nickName", text="Nickname")
+fileTreeview.column("fileName", width=100)
+fileTreeview.column("nickName", width=200)
+fileTreeview.grid(row=2, column=0, padx=10, pady=10, columnspan=1)
 #################################################################
 
 def getPathWork():
-    global directoryWork
+    global directoryWork, textPathWork
     directory = filedialog.askdirectory()
     directoryWork = directory
     filePathWork = open("pathTrabajo.txt", "w")
     filePathWork.write("")
     filePathWork.write(directory)
     if directory:
+        if textPathWork:
+            textPathWork.destroy()
         textPathWork = Label(root, text=directory, font=("Arial", 12), bg="white")
         textPathWork.grid(row=1, column=2, padx=10, pady=10)
 
-buttonPathWork = Button(root, text="Definir el directorio de trabajo",font=("Arial"),width=30, height=2, bg="blue", fg="white", justify="center", command=getPathWork)
+buttonPathWork = Button(root, text="Definir el directorio de trabajo", font=("Arial"), width=30, height=2, bg="blue", fg="white", justify="center", command=getPathWork)
 buttonPathWork.grid(row=0, column=2, padx=10, pady=10)
+
+#################################################################
+
+def downloadFilesConverter():
+    DownloadHTML.downloadHtml(directoryWork)
+
+buttonDownload = Button(root, text="Descargar archivos", font=("Arial"), width=30, height=2, bg="green", fg="white", justify="center", command=downloadFilesConverter)
+buttonDownload.grid(row=0, column=3, padx=10, pady=10)
 
 #################################################################
 
 selectedTag = StringVar(root)
 selectedTag.set("Seleccionar Filtro")
-tagMenu = OptionMenu(root,selectedTag, *tagColumns, command=lambda _: updateFileList(selectedTag.get(), selectedSubTag.get()))
+tagMenu = OptionMenu(root, selectedTag, *tagColumns, command=lambda _: updateFileList(selectedTag.get(), selectedSubTag.get()))
 tagMenu.grid(row=3, column=0, padx=10, pady=10)
 
 selectedSubTag = StringVar(root)
@@ -109,7 +126,76 @@ selectedSubTag.set("Seleccionar SubFiltro")
 subTagMenu = OptionMenu(root, selectedSubTag, *tagSubColumns, command=lambda _: updateFileList(selectedTag.get(), selectedSubTag.get()))
 subTagMenu.grid(row=4, column=0, padx=10, pady=10)
 #################################################################
+# Menu
 
+def openFile():
+    selected_item = fileTreeview.selection()[0]
+    selectFile = fileTreeview.item(selected_item, "values")[0]
+    filePath = path.join(enterpriseDir, selectFile)
+    startfile(filePath)
+
+def assignNickname():
+    selected_item = fileTreeview.selection()[0]
+    selected_file = fileTreeview.item(selected_item, "values")[0]
+    new_nickname = simpledialog.askstring("Asignar Nickname", "Ingrese el nuevo nickname:")
+    if new_nickname:
+        cursor.execute("UPDATE fileReferences SET nickName = ? WHERE fileName = ?", (new_nickname, selected_file))
+        connectionDataBase.commit()
+        updateFileList()
+
+def assignTag():
+    selected_item = fileTreeview.selection()[0]
+    selected_file = fileTreeview.item(selected_item, "values")[0]
+    new_tag = simpledialog.askstring("Asignar Tag", "Ingrese el nuevo tag:")
+    if new_tag:
+        cursor.execute("UPDATE fileReferences SET tag = ? WHERE fileName = ?", (new_tag, selected_file))
+        connectionDataBase.commit()
+        updateFileList()
+
+def assignSubTag():
+    selected_item = fileTreeview.selection()[0]
+    selected_file = fileTreeview.item(selected_item, "values")[0]
+    new_subTag = simpledialog.askstring("Asignar SubTag", "Ingrese el nuevo subtag:")
+    if new_subTag:
+        cursor.execute("UPDATE fileReferences SET subTag = ? WHERE fileName = ?", (new_subTag, selected_file))
+        connectionDataBase.commit()
+        updateFileList()
+
+def deleteTag():
+    selected_item = fileTreeview.selection()[0]
+    selected_file = fileTreeview.item(selected_item, "values")[0]
+    cursor.execute("UPDATE fileReferences SET tag = ? WHERE fileName = ?", ("", selected_file))
+    connectionDataBase.commit()
+    updateFileList()
+
+def deleteSubTag():
+    selected_item = fileTreeview.selection()[0]
+    selected_file = fileTreeview.item(selected_item, "values")[0]
+    cursor.execute("UPDATE fileReferences SET subTag = ? WHERE fileName = ?", ("", selected_file))
+    connectionDataBase.commit()
+    updateFileList()
+
+def deleteNickName():
+    selected_item = fileTreeview.selection()[0]
+    selected_file = fileTreeview.item(selected_item, "values")[0]
+    cursor.execute("UPDATE fileReferences SET nickName = ? WHERE fileName = ?", ("", selected_file))
+    connectionDataBase.commit()
+    updateFileList()
+
+contextMenu = Menu(root, tearoff=0)
+contextMenu.add_command(label="Abrir archivo", command=openFile)
+contextMenu.add_command(label="Asignar un nickname", command=assignNickname)
+contextMenu.add_command(label="Asignar un tag", command=assignTag)
+contextMenu.add_command(label="Asignar un subtag", command=assignSubTag)
+contextMenu.add_command(label="Eliminar el nickname", command=deleteNickName)
+contextMenu.add_command(label="Eliminar el tag", command=deleteTag)
+contextMenu.add_command(label="Eliminar el subtag", command=deleteSubTag)
+
+def showContextMenu(event):
+    contextMenu.post(event.x_root, event.y_root)
+
+fileTreeview.bind("<Button-3>", showContextMenu)
+#################################################################
 # Este codigo debe estar al final del script
 #RUTAS
 #Directorio guardado para el la ruta del enterprise
@@ -140,7 +226,6 @@ finally:
 try:
     filePathWork = open("pathTrabajo.txt", "r")
     directoryWork = filePathWork.readlines()[0]
-    textPathWork.destroy()
 except(IndexError):
     directoryWork = ""
 except(FileNotFoundError):
@@ -153,6 +238,10 @@ if directoryEnterprise:
     label1 = Label(root, text=directoryEnterprise, font=("Arial", 12), bg="white")
     label1.grid(row=1, column=0, padx=10, pady=10)
 
+if directoryWork:
+    textPathWork = Label(root, text=directoryWork, font=("Arial", 12), bg="white")
+    textPathWork.grid(row=1, column=2, padx=10, pady=10)
+
 if not path.exists(enterpriseDir):
     makedirs(enterpriseDir)
 
@@ -161,6 +250,9 @@ if not path.exists(databaseDir):
 
 if not path.exists(originalDir):
     makedirs(originalDir)
+
+if not path.exists(downloadDir):
+    makedirs(downloadDir)
 
 #BASE DE DATOS
 connectionDataBase = sqlite3.connect("./database/database.db")
